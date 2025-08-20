2eb09821ff020f318c891077cb7fe93070944553
Shneor <mm4471661@gmail.com>
2025-08-20 11:16:24 +0300

    shneor/tools update 3 - html css js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,13 @@ const toolMapping = {
   axe: "tree", // גרזן: עצים
   pickaxe: "rock", // מעדר: סלעים
   shovel: "dirt", // את חפירה: אדמה
-//   shears: "branches" // מספריים: ענפים
+  //   shears: "branches" // מספריים: ענפים
+};
+
+const cursorMapping = {
+  axe: "url('../img/diamond-axe.webp') 16 16, auto",
+  pickaxe: "url('../img/diamond-pickaxe.webp') 16 16, auto",
+  shovel: "url('../img/diamond-shovel.webp') 16 16, auto",
 };
 
 // בחירת כלי
@@ -12,9 +18,14 @@ document.querySelectorAll(".tool").forEach((tool) => {
   tool.addEventListener("click", () => {
     activeTool = tool.classList[1];
 
-    document.querySelectorAll(".tool").forEach((t) => {
-      t.style.borderColor = "#333";
-      tool.style.borderColor = "gold";
+    document.querySelectorAll(".tool").forEach((tool) => {
+      tool.style.borderColor = "#333";
+    });
+
+    tool.style.borderColor = "gold";
+
+    document.querySelectorAll(".square").forEach((el) => {
+      el.style.cursor = cursorMapping[activeTool];
     });
   });
 });
@@ -27,7 +38,17 @@ document.querySelectorAll(".square").forEach((square) => {
     const squareType = square.classList[1];
 
     if (toolMapping[activeTool] === squareType) {
-      square.remove();
+      square.classList.remove(squareType);
     }
+  });
+
+  square.addEventListener("mouseover", () => {
+    if (activeTool) {
+      square.classList.add("highlight");
+    }
+  });
+
+  square.addEventListener("mouseout", () => {
+    square.classList.remove("highlight");
   });
 });
